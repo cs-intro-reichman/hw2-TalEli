a63068d0c3d14b2640913ba0c1165efe792be9f8
tal <tal.ariel@runi.post.ac.il>
2024-11-19 23:15:02 +0200

    hw2 1st attempt

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,50 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+	    int seed = Integer.parseInt(args[0]);
+		String mode = args [1];
+		boolean verbose = mode.equals("v");
+		
+		for (int num = 1; num <= seed; num ++){
+			int collatz = num;
+			int count = 0;
+
+			if (verbose){
+				System.out.print(num + " ");
+			}
+			
+			while (true) {
+				count ++;
+				if (collatz == 1) {
+                    if (verbose) {
+                        System.out.print("4 2 1 "); 
+                    }
+                    count += 3; 
+                    break;
+                }
+
+
+				if (collatz % 2 == 0 ){
+					collatz = collatz/2;
+				}
+				else{
+					collatz = collatz * 3 + 1;
+				}
+				if (verbose){
+					System.out.print(collatz + " ");
+				}
+
+				if (collatz == 1) {
+                    break; 
+                }
+				
+			}
+		
+			if (verbose){
+				System.out.println("(" + (count + 1) + ")");
+			}
+
+		}
+		System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
 	}
 }
--- TimeCalc.java
+++ TimeCalc.java
@@ -1,5 +1,30 @@
 public class TimeCalc {
     public static void main(String[] args) {
-        // Replace this comment with your code
+        int timeHours = Integer.parseInt("" + args[0].charAt(0) + args[0].charAt(1));
+		int timeMinutes = Integer.parseInt("" + args[0].charAt(3)  + args[0].charAt(4) + "");
+        int minutesToAdd = Integer.parseInt(args[1]); 
+
+        int hoursToAdd = minutesToAdd/60;
+        timeHours = timeHours + hoursToAdd; 
+        timeMinutes = timeMinutes + minutesToAdd%60;
+
+        if (timeMinutes > 59 ){
+            timeMinutes = timeMinutes%59;
+            timeHours ++;
+         }
+
+        if (timeHours > 23)
+        {
+            timeHours = timeHours%24;
+        }
+
+
+        String minS = (timeMinutes < 10) ? "0" + String.valueOf(timeMinutes) : String.valueOf(timeMinutes); 
+        String hourS = ( timeHours < 10) ? "0" + String.valueOf(timeHours) : String.valueOf(timeHours); 
+
+        System.out.println(hourS + ":" + minS);
+
+
+
     }
 }
